8dc00a8881619543869e7ef2ba79a062f55bbb7f
gaurav gupta <gauravgupta926099@gmail.com>
2026-08-12 20:21:52 +0530

    Update button.jsx

--- react-projects/backgroundchanger/src/button.jsx
+++ react-projects/backgroundchanger/src/button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Button({ color = "black", onClick }) {
 
-    let p = `rounded-xl text-black pl-4 pr-4 flex items-center justify-center `;
+    let p = `rounded-xl text-orange-300 pl-4 pr-4 flex items-center justify-center `;
     return (<button className={p} onClick={onClick} style={{ backgroundColor: color }}>{color}</button>)
 }
 export default Button;
